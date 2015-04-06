89f58d67bdafc6a0c9cfadf67d0424c2bea5d155
hobbe84 <impreza99>
2015-04-06 13:41:47 +0200

    Big Push

---
+++ Labb 4/Libary/Libary/Models/CustomerSummary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Libary.DataAccess;
+using System.Web;
+using System.ComponentModel;
+
+namespace Libary.Models
+{
+    public class CustomerSummary
+    {
+        [DisplayName("Allowed to Loan")]
+        public bool EntitledForLoan { get; set; }
+        public int CustomerID { get; set; }
+        [DisplayName("Customer Name")]
+        public string CustomerName { get; set; }
+        [DisplayName("Streetaddress")]
+        public string Address { get; set; }
+        [DisplayName("Phonenumber")]
+        public string PhoneNumber { get; set; }
+        [DisplayName("Emailaddress")]
+        public string EmailAddress { get; set; }
+        public string Gender { get; set; }
+        [DisplayName("Birth Date")]
+        public DateTime? BirthDate { get; set; }
+        [DisplayName("No of Active Loans")]
+        public int LitteratureLoans { get; set; }
+        [DisplayName("Books Overdue")]
+        public int LoansOverDue { get; set; }
+        [DisplayName("Active Loans")]
+        public bool ActiveLoans { get; set; }
+    }
+}
